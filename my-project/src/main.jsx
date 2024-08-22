979bb9bc1d25594f8ac7e4ff27868392df1cfdf5
UmerIshaq <140231923+UmerIshaq@users.noreply.github.com>
2024-08-22 18:25:53 +0300

    vercel speed insight added

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { MouseFollower } from "react-mouse-follower";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MouseFollower></MouseFollower>
+    <SpeedInsights />
     <App />
   </StrictMode>
 );
